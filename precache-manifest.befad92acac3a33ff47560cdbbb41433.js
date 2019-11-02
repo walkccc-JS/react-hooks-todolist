39e44b4d8b4efa3943d299fd2ae590db55932857
Jay Chen <walkccray@gmail.com>
2019-11-03 02:48:12 +0800

    Updates

---
+++ precache-manifest.befad92acac3a33ff47560cdbbb41433.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "630d396f46f3c016b34dc864e7c6ab56",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e4a609f4f8ebea6a2b9",
+    "url": "/static/css/main.a0b496bd.chunk.css"
+  },
+  {
+    "revision": "47a8513a079b95b977ab",
+    "url": "/static/js/2.1c61302b.chunk.js"
+  },
+  {
+    "revision": "3e4a609f4f8ebea6a2b9",
+    "url": "/static/js/main.3e6ed0e8.chunk.js"
+  },
+  {
+    "revision": "630881b71863c6a4668b",
+    "url": "/static/js/runtime-main.c5569319.js"
+  }
+]);
